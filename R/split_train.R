d0741b1491605cfb711ab6966c61bc1bd0d78c4c
awqx <awqx@users.noreply.github.com>
2020-08-29 15:51:04 -0400

    testing documentation for a simple function

--- R/split_train.R
+++ R/split_train.R
@@ -1,6 +1,12 @@
-split_train <- function(trn, nfolds, seed) {
+#' Create \code{num} splits in training data saved in a single list
+#'
+#' @param num The number of folds created
+#' @param seed The random seed, for reproducibility
+#' @return A list containing \code{num} folds of the training set data
+
+split_train <- function(trn, num, seed) {
   set.seed(seed)
-  fold_list <- createFolds(trn$dG, k = nfolds)
+  fold_list <- createFolds(trn$dG, k = num)
   lapply(
     fold_list,
     function(x, dat) list("trn" = dat[-x, ], "tst" = dat[x, ]),
