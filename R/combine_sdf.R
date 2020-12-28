73d4ca9d42fd04994d03cee423c3cf6b384b3ce4
awqx <awqx@users.noreply.github.com>
2020-12-28 13:23:27 -0500

    Fixed `combine_sdf` to ignore empty directories

--- R/combine_sdf.R
+++ R/combine_sdf.R
@@ -2,14 +2,30 @@
 #'
 #' `combine_sdf` combines a directory of SDFs into a single SDF.
 #' For each molecule in the single SDF, the molecule name is the filename.
+#' If the directory is empty, the function returns a 0.
+#' The function will message if a directory is processed. This
+#' can be suppressed with `quiet = T`.
 #'
 #' @param mol_dir The directory containing the SDFs to combine.
+#' @param quiet Whether or not the function will message when a directory
+#'   is processed. The default is `quiet = FALSE`.
 #' @return An SDF as a data frame with one column.
 #' @import dplyr
 #' @importFrom stringr str_detect str_remove
 #' @export
 
-combine_sdf <- function(mol_dir) {
+combine_sdf <- function(mol_dir, quiet = F) {
+  # Exit if directory is empty
+  if (!length(list.files(mol_dir))) {
+    if (!quiet) {
+      message(
+        "The directory ", mol_dir, " is empty.", "\n",
+        "Skipping ", mol_dir, " and returning 0."
+      )
+    }
+    return(0)
+  }
+
   mol_files <- list.files(mol_dir, full.names = T) %>%
     .[stringr::str_detect(., "(?i)sdf")]
   mol_names <- list.files(mol_dir) %>%
@@ -30,9 +46,12 @@ combine_sdf <- function(mol_dir) {
     stringsAsFactors = F
     )
 
-  # Assigning the correct names
+  # Assign the correct names
   for(i in 1:length(sdf_list)) sdf_list[[i]][1, ] <- mol_names[i]
 
-  # return combined list
+  # Message that directory is processed
+  if (!quiet) message("The directory", mol_dir, " was processed.")
+
+  # Return combined list
   do.call(rbind, sdf_list)
 }
