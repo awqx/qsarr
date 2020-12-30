75412e401bc5fa6b77423a05a1755575a13ab6d2
awqx <awqx@users.noreply.github.com>
2020-12-30 14:21:29 -0500

    Option to remove filename

--- R/combine_sdf.R
+++ R/combine_sdf.R
@@ -2,6 +2,7 @@
 #'
 #' `combine_sdf` combines a directory of SDFs into a single SDF.
 #' For each molecule in the single SDF, the molecule name is the filename.
+#' Optionally, molecule names can be removed with `rm_name = T`.
 #' If the directory is empty, the function returns a 0.
 #' The function will message if a directory is processed. This
 #' can be suppressed with `quiet = T`.
@@ -9,12 +10,14 @@
 #' @param mol_dir The directory containing the SDFs to combine.
 #' @param quiet Whether or not the function will message when a directory
 #'   is processed. The default is `quiet = FALSE`.
+#' @param rm_name Whether or not to remove the filename. The default is
+#'   `rm_name = FALSE`.
 #' @return An SDF as a data frame with one column.
 #' @import dplyr
 #' @importFrom stringr str_detect str_remove
 #' @export
 
-combine_sdf <- function(mol_dir, quiet = F) {
+combine_sdf <- function(mol_dir, quiet = F, rm_name = F) {
   # Exit if directory is empty
   if (!length(list.files(mol_dir))) {
     if (!quiet) {
@@ -28,15 +31,12 @@ combine_sdf <- function(mol_dir, quiet = F) {
 
   mol_files <- list.files(mol_dir, full.names = T) %>%
     .[stringr::str_detect(., "(?i)sdf")]
-  mol_names <- list.files(mol_dir) %>%
-    stringr::str_remove("(?i)\\.sdf")
 
   # Removing empty files
   mol_info  <- lapply(mol_files, file.info) %>%
     lapply(function(x) x$size > 30) %>%
     unlist()
   mol_files <- mol_files[mol_info]
-  mol_names <- mol_names[mol_info]
 
   # Reading into a list
   sdf_list <- lapply(
@@ -44,13 +44,26 @@ combine_sdf <- function(mol_dir, quiet = F) {
     read.csv,
     header = F,
     stringsAsFactors = F
-    )
+  )
 
   # Assign the correct names
-  for(i in 1:length(sdf_list)) sdf_list[[i]][1, ] <- mol_names[i]
+  # Notice that the numbers are being subset
+  # This will make obtaining the names easier later
+  mol_names <- if (rm_name) {
+    c(1:length(list.files(mol_dir)))
+  } else {
+    list.files(mol_dir) %>%
+      stringr::str_remove("(?i)\\.sdf")
+  }
+
+  mol_names <- mol_names[mol_info]
+
+  for (i in 1:length(sdf_list)) {
+    sdf_list[[i]][1, ] <- mol_names[i]
+  }
 
   # Message that directory is processed
-  if (!quiet) message("The directory", mol_dir, " was processed.")
+  if (!quiet) message("The directory ", mol_dir, " was processed.")
 
   # Return combined list
   do.call(rbind, sdf_list)
